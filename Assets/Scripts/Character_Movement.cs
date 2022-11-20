3efa973d352f99d185ae42339b19f3705b5be02f
Dev <d.trivedi@saedubai.com>
2022-11-20 12:47:30 +0400

    Animation setup

---
+++ Assets/Scripts/Character_Movement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_Movement : MonoBehaviour
+{
+	float jumpHeight;
+	float runSpeed;
+	
+	void Update()
+	{
+    
+	}
+	void Inputs()
+	{
+
+	}
+}
