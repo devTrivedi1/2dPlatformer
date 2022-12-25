96df3a3e5d9f0cfd4268128ba46c1b7e6db47846
Dev <d.trivedi@saedubai.com>
2022-12-25 15:30:54 +0400

    ANIMATION FLIPS LEFT OR RIGHT NOW
    no jump tho

--- Assets/Scripts/Character_Movement.cs
+++ Assets/Scripts/Character_Movement.cs
@@ -4,15 +4,62 @@ using UnityEngine;
 
 public class Character_Movement : MonoBehaviour
 {
-	float jumpHeight;
-	float runSpeed;
-	
+	[SerializeField] float jumpHeight;
+	[SerializeField] float runSpeed;
+	float x;
+	float y;
+
+	Animator animator;
+	Rigidbody2D rb;
+	SpriteRenderer[] sprites;
+	bool defaultFacingRight = true;
+	bool facingRight = true;
+
+	void Start()
+	{
+		animator = GetComponentInChildren<Animator>();
+		rb = GetComponent<Rigidbody2D>();
+		sprites = GetComponentsInChildren<SpriteRenderer>();
+	}
 	void Update()
 	{
-    
-	}
-	void Inputs()
-	{
+		Inputs();
+		Move();
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			rb.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+			animator.SetBool("Jumping", true);
+		}
 
 	}
+
+
+	void Inputs()
+	{
+		x = Input.GetAxisRaw("Horizontal") * runSpeed;
+		y = Input.GetAxisRaw("Vertical");
+		animator.SetFloat("Running", Mathf.Abs(x));
+	}
+	void Move()
+	{
+		float yPos = transform.position.y;
+		Vector3 movement = new Vector2(x, yPos);
+		movement = Vector3.ClampMagnitude(movement, 1);
+		rb.velocity = movement.normalized * runSpeed;
+		if (movement.x < 0)
+		{
+			foreach (SpriteRenderer rend in sprites)
+			{
+				rend.flipX = (defaultFacingRight != facingRight);
+			}
+		}
+		else if (movement.x > 0)
+		{
+			foreach (SpriteRenderer rend in sprites)
+			{
+				rend.flipX = (defaultFacingRight = facingRight);
+			}
+		}
+	}
+
 }
